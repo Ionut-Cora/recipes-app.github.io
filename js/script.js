85e03f4140664eafa5bb74c7be335efd5b79989c
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-24 18:13:07 +0000

    card style change

--- js/script.js
+++ js/script.js
@@ -51,46 +51,50 @@ function displayReceipe(data) {
         content.innerHTML += `
             <div class="card">
                 <h2>${hit.recipe.label}</h2>
-                <img src="${hit.recipe.image}">
-                <div class="content-div">
-                    <p><span>Calories: </span>${hit.recipe.calories.toPrecision(6)}kcal</p>
-                    <div class="diet-div">
-                        <p><span>Diet label: </span></p>
-                        <div>
-                            ${
-                                hit.recipe.dietLabels.map(dietLabel => {
-                                    return `<p>${dietLabel}</p>`;
-                                })
-                            }                              
+                <div class="card-content">
+                    <div class="image-div">
+                        <img src="${hit.recipe.image}" alt="recipe image">
+                    </div>
+                    <div>
+                        <div class="content-div">
+                            <p><span>Calories: </span>${hit.recipe.calories.toPrecision(6)}kcal</p>
+                            <div class="diet-div">
+                                <p><span>Diet label: </span></p>
+                                <div>
+                                    ${
+                                        hit.recipe.dietLabels.map(dietLabel => {
+                                            return `<p>${dietLabel}</p>`;
+                                        })
+                                    }                              
+                                </div>
+                            </div>
+                            <div class="health-div">
+                                <div class="health-div__items">
+                                    <p><span>Health label: </span></p>
+                                    ${
+                                        hit.recipe.healthLabels.map(healthLabel => {
+                                            return `<p class="health-label">${healthLabel}</p>`;
+                                        })
+                                    }                              
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ingredients-div">
+                            <div class="ingredients-div__items">
+                                <p><span>Ingredients:</span></p>
+                                ${
+                                    hit.recipe.ingredientLines.map(ingredient => {
+                                        return `<p class="ingredient-label">${ingredient}</p>`;
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div class="view-recipe-div">               
+                            <button class="custom-btn recipe-btn">
+                                <a href="${hit.recipe.url}" target="_blank">View Recipe</a>                     
+                            </button>
                         </div>
                     </div>
-                    <div class="health-div">
-                        <div class="health-div__items">
-                            <p><span>Health label: </span></p>
-                            ${
-                                hit.recipe.healthLabels.map(healthLabel => {
-                                    return `<p class="health-label">${healthLabel}</p>`;
-                                })
-                            }                              
-                        </div>
-                    </div>
-                </div>
-                <div class="ingredients-div">
-                    <details>
-                        <summary>Ingredients:</summary>
-                        <ul id="taglist">
-                            ${
-                                hit.recipe.ingredientLines.map(ingredient => {
-                                    return `<li>${ingredient}</li>`;
-                                })
-                            }                                        
-                        </ul>
-                    </details>
-                </div>
-                <div class="view-recipe-div">               
-                    <button class="custom-btn recipe-btn">
-                        <a href="${hit.recipe.url}" target="_blank">View Recipe</a>                     
-                    </button>
                 </div>
             </div>
         `;
